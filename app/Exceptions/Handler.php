40fb44589049d91c0282e0da4aee7dfe64236236
Konstantin <konstantinzirjanov@gmail.com>
2016-10-12 15:01:06 +0400

    Some bugs fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($exception->getStatusCode()===404)
+        if($exception instanceof NotFoundHttpException)
         {
             return response()->view('errors.404', [], 404);
         }
